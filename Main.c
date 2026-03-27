5740980de7d2a3d081c586606eba6b7c041387c4
Rishabh <rishabhji@zohomail.in>
2026-03-27 10:56:37 +0530

    Add initial implementation of various C programs

---
+++ Main.c
@@ -0,0 +1,221 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+/*{
+		int a,b,sum;
+		printf("enter value of a");
+		scanf("%i",&a);
+		printf("enter of value b");
+		scanf("%i",&b);
+		sum=a+b;
+		printf("the sum of %i ",sum);
+		getch();
+		return 0;
+}
+	float a,b,sum,sub,mul,div;
+	char op;
+	printf("Ã«nter the first no.");
+	scanf("")
+	
+{
+		int a;
+	printf("enter the number");
+	scanf("%i",&a);
+	if (a%2==0)
+	{
+		printf("Ã«ven no.");
+	}
+	else {
+		printf("Ã¶dd no.");
+}
+
+}
+
+{
+	int	a;
+	printf("Ã«nter the number");
+	scanf("%i" ,&a);
+	if (a>0)
+	
+	{
+		printf("positive number");
+		}
+		else if (a==0)
+		{
+		printf("zero");
+		}
+		else
+		{ printf("negative number");
+		}
+}
+{
+		int a,b,c;
+	printf("enter the first number");
+	scanf("%i",&a);
+	printf("enter second no.");
+	scanf("%i",&b);
+	if(a>b)
+	{
+		printf("a is greater");
+	}
+	else if (a==b)
+	{
+		printf("equal no.");
+	}
+	else
+	{
+		printf("b is greater");
+	}
+}
+	{
+		int a,b,c;
+		printf("enter the first number");
+		scanf("%i",&a);
+		printf("enter the second no.");
+		scanf("%i",&b);
+		printf("enter the third no.");
+		scanf("%i",&c);
+		if (a>b&&a>c)
+		{
+			printf("a is greater then b and c");
+		}
+		else if (b>c&&b>a)
+		{
+			printf("b is greater than a and c");
+		}
+		else if(c>a&&c>b)
+		{
+			printf("c is greater than a and b");
+		}
+		else if (a==b&&b==c)
+		{
+			printf("equal no.");
+		}
+	}
+		{
+			int sub1,sub2,sub3,sub4,sub5;
+			float avg;
+			printf("enter the mark of sub1");
+			scanf("%i",&sub1);
+			printf("enter the mark of sub2");
+			scanf("%i",&sub2);
+			printf("enter the mark of sub3");
+			scanf("%i",&sub3);
+			printf("enter the mark of sub4");
+			scanf("%i",&sub4);
+			printf("enter the mark of sub5");
+			scanf("%i",&sub5);
+			avg=(sub1+sub2+sub3+sub4+sub5)/5;
+			printf("the percent of marks is %.2f",avg,"%");
+			if(avg>35)
+			{
+				printf("\nyou are pass");
+			}
+			else{
+				printf("\nfail");
+			}
+		
+	    	{
+	    	printf("to print odd no 1 to 93 \n");
+	    	int i=2;
+			while (i<=10)
+			{
+				printf("%d \n",i);
+				i+=2;
+				}	
+			}
+			
+			{
+			int n;
+			long int fact;
+	    	printf("to print odd no 1 to 93 \n");
+	    	int i=2;
+			while (i<=10)
+			{
+				printf("%d \n",i);
+				i+=2;
+				}	
+			}
+			
+			{
+				printf("to calculate the factorial of a given \n");
+				int i,n;
+				long int fact=1;
+				printf("enter the given number");
+				scanf("%i",&n);
+				for(i=1;i<=n;i++)
+				fact=fact*i;
+				printf("the factorial of %d is %1d\n",n,fact);
+				
+			}
+			
+		{
+		int a=10,b=10,c=23,r1,r2,r3,r4;
+		r1=(a==b)&&(a<c);
+		printf("output of (a==b)&&(a<c)is %d \n",r1);
+		r2=(a==b)&&(a>c);
+		printf("output of (a==b)&&(a>c)is %d \n",r2);	
+		r3=(a==b)||(a<c);	
+		printf("output of (a==b)||(a<c)is %d \n",r3);
+		r4=(a!=b);
+		printf("output of (a!=b) is %d \n",r4);	
+			
+		}
+		{
+		int day;
+		printf("enter the day number");
+		scanf("%d",&day);
+		switch (day)
+		{
+			case 1:
+				printf("monday");
+				break;
+			case 2:
+				printf("tuseday");
+				break;
+			case 3:
+				printf("wednesday");
+				break;
+			case 4:
+				printf("thursday");
+				break;
+			case 5:
+				printf("friday");
+				break;
+			case 6: gbxrf ftnht
+					printf("saturday");
+					break;
+				}	
+		
+		}*/
+
+// #include <stdio.h>
+
+// void main() 
+// {
+    
+//         int marks [4] = { 15 , 16 ,23 ,03 };
+        
+//         int i;
+        
+//         for (i=0;i<4;i++)
+
+//           { 
+//               printf("\n%d", marks[i] );
+             
+//           }
+     
+// }
+
+
+
+{
+		
+  
+  
+  
+  
+}
+  
+  
+  
